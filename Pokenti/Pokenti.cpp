391eee51c173ea8acd93dc047b1f446b85e18d08
WalterScherk <44490381+WalterScherk@users.noreply.github.com>
2024-05-14 12:37:08 +0200

    colocar_y_recolocar_pokeballs
    Aún no se aumenta el número de pokeballs

--- Pokenti/Pokenti.cpp
+++ Pokenti/Pokenti.cpp
@@ -20,6 +20,10 @@ struct Pokemon
     int posY;
 };
 
+struct Pokeball {
+    int posX = 2;
+    int posY = 2;
+};
 
 void PutPlayer(Player& me, int width, char *map[]) {
 
@@ -42,8 +46,19 @@ bool PutPokemon(Pokemon pokemons[], int& allPokemonAround, int& posX, int& posY)
     return false;
 }
 
+bool PutPokeball(Pokeball pokeballs[], int& allPokeballAround, int& posX, int& posY) {
+
+    for (int a = 0; a < allPokeballAround; a++) {
 
 
+        if (pokeballs[a].posX == posX && pokeballs[a].posY == posY) {
+
+            return true;
+        }
+    }
+
+    return false;
+}
 
 void CheckNumbers(std::string linea, int& leftNum, int& rightNum, bool& leftNumber) {
 
@@ -105,8 +120,12 @@ int main()
     int pokemonAround2 = 0;
     int pokemonNeeded2 = 0;
 
-    
-
+    int maxPokeballs = 12;
+    Pokeball *pokeballs = new Pokeball[maxPokeballs];
+    /*for (int i = 0; i < maxPokeballs; i++)
+    {
+        pokeballs[i] = new Pokeball;
+    }*/
     
 
 
@@ -156,8 +175,7 @@ int main()
     for (int i = pokemonAround1; i < pokemonAround1 + pokemonAround2; i++)
     {
         pokemons[i].posX = rand() % ((width / 2) - 1);
-        pokemons[i].posY = ((height / 2) + 1) + (rand() % ((height / 2) - 1));
-        std::cout << pokemons[i].posX << ' ' << pokemons[i].posY << std::endl; 
+        pokemons[i].posY = ((height / 2) + 1) + (rand() % ((height / 2) - 1)); 
     }
 
 
@@ -225,9 +243,9 @@ int main()
             if (me.GetX() - 1 >= 0 && map[me.GetY()][me.GetX() - 1] == '-')
                 me.SetPosition(me.GetX() - 1, me.GetY());
         }
-            
-
-            
+        //hacer una función para las teclas de movimiento
+        //poner return 1   
+        // si es return 1, comprovar todas las pokeball
         else if (GetAsyncKeyState(VK_SPACE)) {
 
             if (me.GetDir() == '<')
@@ -264,7 +282,26 @@ int main()
 
         }
 
-        
+        for (int i = 0; i < maxPokeballs; i++) {
+            if (me.GetX() == pokeballs[i].posX && me.GetY() == pokeballs[i].posY) {
+                if (i < maxPokeballs / 4) {
+                    pokeballs[i].posX = rand() % ((width / 2) - 1);
+                    pokeballs[i].posY = rand() % ((height / 2) - 1);
+                }
+                else if (i < maxPokeballs / 2) {
+                    pokeballs[i].posX = rand() % ((width / 2) - 1);
+                    pokeballs[i].posY = ((height / 2) + 1) + (rand() % ((height / 2) - 1));
+                }
+                else if (i < (maxPokeballs / 4) * 3) {
+                    pokeballs[i].posX = ((width / 2) + 1) + (rand() % ((width / 2) - 1));
+                    pokeballs[i].posY = ((height / 2) + 1) + (rand() % ((height / 2) - 1));
+                }
+                else {
+                    pokeballs[i].posX = ((width / 2) + 1) + (rand() % ((width / 2) - 1));
+                    pokeballs[i].posY = rand() % ((height / 2) - 1);
+                }
+            }
+        }
 
         //GET ALL CHARS from array TO 0
 
@@ -300,6 +337,8 @@ int main()
                     PutPlayer(me, width, map);
                 else if (PutPokemon(pokemons, allPokemonAround, i, j))
                     std::cout << 'P';
+                else if (PutPokeball(pokeballs, maxPokeballs, i, j))
+                    std::cout << 'O';
                 else
                     std::cout << map[j][i];
 
